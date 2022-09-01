808e589da6c9f0a4158003702ba2fc8badeb629b
Hawri <hawrikazaz@gmail.com>
2022-09-01 21:33:43 +0100

    reminders

--- new-react-project/src/App.js
+++ new-react-project/src/App.js
@@ -31,11 +31,25 @@ const deleteTask = (id) => {
   id))
 }
 
+//Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) =>
+     task.id === id ? { ...task, reminder:
+       !task.reminder } : task
+       )
+       )
+}
+
   return (
     <div className='container'>
       <Header  />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete=
-      {deleteTask} /> : 'No Tasks'}
+      {tasks.length > 0 ? (
+      <Tasks tasks={tasks} onDelete=
+      {deleteTask} onToggle={toggleReminder} />
+      ) : (
+        'No Tasks'
+      )}
     </div>
   );
 }
