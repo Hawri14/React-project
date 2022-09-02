aeb7122707e9dde0f0c3116cf081a2b9044d4ed5
Hawri <hawrikazaz@gmail.com>
2022-09-02 21:33:38 +0100

    added task submit and states

--- new-react-project/src/App.js
+++ new-react-project/src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -26,6 +27,14 @@ const App = () => {
 },
 ])
 
+//Add Task
+const addTask = (task) => {
+  const id = Math.floor(Math.random() *10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+  
+}
+
 //delete tasks
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !==
@@ -44,8 +53,8 @@ const toggleReminder = (id) => {
 
   return (
     <div className='container'>
-      <Header  />
-      <AddTask />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
       <Tasks tasks={tasks} onDelete=
       {deleteTask} onToggle={toggleReminder} />
