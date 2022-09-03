b95c0a8111af4de76a9bbbcddcb9197aa7f22b92
Hawri <hawrikazaz@gmail.com>
2022-09-03 18:05:04 +0100

    Delete, Add and reminder to server

--- new-react-project/src/App.js
+++ new-react-project/src/App.js
@@ -1,4 +1,4 @@
-//to run erver do npm start in one termilan and in another do npm run server
+//to run everything, do npm start in one terminal and in another do npm run server
 
 import { useState, useEffect } from 'react'
 import Header from "./components/Header";
@@ -26,28 +26,65 @@ const App = () => {
     return data
   }
 
+    // fetch the data 
+    const fetchTask = async (id) => {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`) 
+      const data = await res.json()
+  
+      return data
+    }
+
 //Add Task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() *10000) + 1
-  const newTask = { id, ...task }
-  setTasks([...tasks, newTask])
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:5000/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+
+  const data = await res.json()
+
+  setTasks([...tasks, data])
+
+ // const id = Math.floor(Math.random() *10000) + 1
+  //const newTask = { id, ...task }
+  //setTasks([...tasks, newTask])
   
 }
 
 //delete tasks
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'DELETE'
+  })
+
   setTasks(tasks.filter((task) => task.id !==
   id))
 }
 
 //Toggle Reminder
-const toggleReminder = (id) => {
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id)
+  const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(updTask)
+  })
+
+  const data = await res.json()
+
   setTasks(
     tasks.map((task) =>
      task.id === id ? { ...task, reminder:
-       !task.reminder } : task
+       data.reminder } : task
        )
-       )
+    )
 }
 
   return (
